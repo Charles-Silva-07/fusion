43d9277ff3ab0486beeaeedfcae456ad69f8c19f
Charles-Silva-07 <charllesrocklp@gmail.com>
2023-09-26 18:38:26 -0300

    testando deploy2

--- fusion/settings.py
+++ fusion/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fusion',
+        'USER': 'postgres',
+        'PASSWORD': 'nasa0147',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# MEDIA_URL = 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # MEDIA_URL = 'media/'
 
 # Default primary key field type
